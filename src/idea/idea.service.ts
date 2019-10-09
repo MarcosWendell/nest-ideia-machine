edeb021253a1016f91d6f7f3c018c16ca745ecd9
Marcos Wendell Santos <mwsantos_token@gmail.com>
2019-10-09 16:58:04 -0300

    Added validation and logging

--- src/idea/idea.service.ts
+++ src/idea/idea.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -22,16 +22,32 @@ export class IdeaService {
   }
 
   async read(id: string) {
-      return await this.ideaRepository.findOne({ where: { id } });
+    const idea =  await this.ideaRepository.findOne({ where: { id } });
+
+    if (!idea) {
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    }
+    return idea;
   }
 
   async update(id: string, data: Partial<IdeaDTO>) {
-    await this.ideaRepository.update({ id }, data);
-    return await this.ideaRepository.findOne({ id });
+    const idea =  await this.ideaRepository.findOne({ where: { id } });
+
+    if (!idea) {
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    }
+
+    return await this.ideaRepository.update({ id }, data);
   }
 
   async remove(id: string) {
+    const idea =  await this.ideaRepository.findOne({ where: { id } });
+
+    if (!idea) {
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    }
+
     await this.ideaRepository.delete({ id });
-    return { deleted: true };
+    return idea;
   }
 }
--- src/shared/logging.interceptor.ts
+++ src/shared/logging.interceptor.ts
@@ -3,28 +3,28 @@ import {
   NestInterceptor,
   ExecutionContext,
   Logger,
+  CallHandler,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  intercept(
-    context: ExecutionContext,
-    call$: Observable<any>,
-  ): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const req = context.switchToHttp().getRequest();
     const method = req.method;
     const url = req.url;
     const now = Date.now();
 
-    return call$.pipe(
-      tap(() =>
-        Logger.log(
-          `${method} ${url} ${Date.now() - now}`,
-          context.getClass().name,
+    return next
+      .handle()
+      .pipe(
+        tap(() =>
+          Logger.log(
+            `${method} ${url} ${Date.now() - now}`,
+            context.getClass().name,
+          ),
         ),
-      ),
-    );
+      );
   }
 }
